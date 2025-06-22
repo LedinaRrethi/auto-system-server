c7409f035e961265f8b1e4bb7fb4af9a34f6c393
Ledina Rrethi <ledinarrethi@gmail.com>
2025-06-22 11:43:11 +0200

    feat: Expand Auto_Directorates seeder with more cities

--- DAL/Seed/DirectorateSeeder.cs
+++ DAL/Seed/DirectorateSeeder.cs
@@ -9,26 +9,24 @@ namespace DAL.Seed
         {
             if (!await context.Auto_Directorates.AnyAsync())
             {
+                var creatorId = "b103da7f-891a-45a5-a1c1-00cafba110c0";
+                var createdOn = DateTime.UtcNow;
+                var createdIp = "127.0.0.1";
+
                 var defaultDirectorates = new List<Auto_Directorates>
                 {
-                    new Auto_Directorates
-                    {
-                        IDPK_Directory = Guid.NewGuid(),
-                        DirectoryName = "Tirana",
-                        Address = "Rruga e Dibrës",
-                        CreatedBy = "b103da7f-891a-45a5-a1c1-00cafba110c0", 
-                        CreatedOn = DateTime.UtcNow,
-                        CreatedIp = "127.0.0.1"
-                    },
-                    new Auto_Directorates
-                    {
-                        IDPK_Directory = Guid.NewGuid(),
-                        DirectoryName = "Durrës",
-                        Address = "Sheshi Liria",
-                        CreatedBy = "b103da7f-891a-45a5-a1c1-00cafba110c0", 
-                        CreatedOn = DateTime.UtcNow,
-                        CreatedIp = "127.0.0.1"
-                    }
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Tirana", Address = "Rruga e Dibrës", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Durrës", Address = "Sheshi Liria", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Shkodra", Address = "Lagjja Parrucë", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Elbasan", Address = "Rruga Qemal Stafa", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Fier", Address = "Rruga Jakov Xoxa", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Vlorë", Address = "Rruga Kosova", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Korçë", Address = "Rruga Republika", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Gjirokastër", Address = "Rruga e Zejtareve", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Berat", Address = "Rruga Antipatrea", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Dibër", Address = "Rruga e Çerçiz Topulli, Peshkopi", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Kukës", Address = "Rruga Fan S. Noli", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp },
+                    new Auto_Directorates { IDPK_Directory = Guid.NewGuid(), DirectoryName = "Lezhë", Address = "Rruga Besëlidhja", CreatedBy = creatorId, CreatedOn = createdOn, CreatedIp = createdIp }
                 };
 
                 await context.Auto_Directorates.AddRangeAsync(defaultDirectorates);
