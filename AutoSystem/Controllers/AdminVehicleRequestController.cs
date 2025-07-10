8c65f5317333a3d911bc055fce70b93b8b6dd1e3
Ledina Rrethi <ledinarrethi@gmail.com>
2025-07-10 21:18:28 +0200

    refactor: A better info ms

--- AutoSystem/Controllers/AdminVehicleRequestController.cs
+++ AutoSystem/Controllers/AdminVehicleRequestController.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
         {
             var result = await _domain.GetAllRequestsAsync(dto);
 
-            result.Message = !result.Items.Any() ? "You have no vehicles." : "Success";
+            result.Message = !result.Items.Any() ? "You have no requests." : "Success";
 
             return Ok(result);  
         }
