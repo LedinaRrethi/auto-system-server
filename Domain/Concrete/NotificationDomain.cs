743f23adf428a9e7bdd53126e9b41a51dae7abb1
Ledina Rrethi <ledinarrethi@gmail.com>
2025-07-21 10:20:05 +0200

    fix

--- Domain/Concrete/NotificationDomain.cs
+++ Domain/Concrete/NotificationDomain.cs
@@ -1,6 +1,8 @@
 ﻿using DAL.UoW;
 using Domain.Contracts;
 using DTO.NotificationDTOs;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Domain.Concrete
 {
@@ -8,15 +10,24 @@ namespace Domain.Concrete
     {
         private readonly INotificationRepository _notificationRepository;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<Auto_Users> _userManager;
 
-        public NotificationDomain(INotificationRepository notificationRepository, IUnitOfWork unitOfWork)
+        public NotificationDomain(INotificationRepository notificationRepository, IUnitOfWork unitOfWork, UserManager<Auto_Users> userManager)
         {
             _notificationRepository = notificationRepository;
             _unitOfWork = unitOfWork;
+            _userManager = userManager;
         }
 
         public async Task<List<NotificationDTO>> GetAllNotificationsAsync(string userId)
         {
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return new List<NotificationDTO>();
+
+            var isIndivid = await _userManager.IsInRoleAsync(user, "Individ");
+            if (!isIndivid) return new List<NotificationDTO>();
+
             var notifications = await _notificationRepository.GetAllNotificationsUserAsync(userId);
 
             return notifications.Select(n => new NotificationDTO
@@ -34,6 +45,12 @@ namespace Domain.Concrete
 
         public async Task<List<NotificationDTO>> GetUnseenNotificationsAsync(string userId)
         {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return new List<NotificationDTO>();
+
+            var isIndivid = await _userManager.IsInRoleAsync(user, "Individ");
+            if (!isIndivid) return new List<NotificationDTO>();
+
             var notifications = await _notificationRepository.GetAllNotificationsUnseenAsync(userId);
 
             return notifications.Select(n => new NotificationDTO
@@ -51,6 +68,11 @@ namespace Domain.Concrete
 
         public async Task<int> CountUnseenNotificationsAsync(string userId)
         {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null || !(await _userManager.IsInRoleAsync(user, "Individ")))
+                return 0;
+
             return await _notificationRepository.CountUnseenNotificationsAsync(userId);
         }
 
